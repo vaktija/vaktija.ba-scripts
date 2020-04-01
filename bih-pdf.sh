70b2a8166ae6e4fbfba2175d77d1a26303dc7b96
info@vaktija.ba <info@vaktija.ba>
2020-04-01 14:07:22 +0200

    2020 Dump with hijri and DST

--- bih-pdf.sh
+++ bih-pdf.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# usage
+# ./bih-pdf.sh 'http://192.168.0.20/print.php?location='
+#
 for i in {1..107}
 do
   mkdir $i
@@ -7,7 +10,7 @@ do
     do
       grad=$i
       mjesec=$j
-      url='http://192.168.0.20/print.php?location='$grad'&'month=''$mjesec''
+      url=$1$grad'&'month=''$mjesec''
       echo $url
       wkhtmltopdf --orientation Portrait --viewport-size 1366x768 --page-size A4 $url "$grad"_"$mjesec"_2020.pdf
     done
--- sandzak-pdf.sh
+++ sandzak-pdf.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# usage
+# ./sandzak-pdf.sh 'http://192.168.0.20/print.php?location='
+#
 for i in `seq 11`
 do
   mkdir $i
@@ -8,7 +11,7 @@ do
       grad=$i
       grad1000=$(($grad * 1000))
       mjesec=$j
-      url='http://192.168.0.20/print.php?location='$grad1000'&'month=''$mjesec''
+      url=$1$grad1000'&'month=''$mjesec''
       echo $url
       wkhtmltopdf --orientation Portrait --viewport-size 1366x768 --page-size A4 $url "$grad"_"$mjesec"_2020.pdf
     done
